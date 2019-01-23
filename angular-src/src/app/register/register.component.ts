29ca4afbbeb6063becbc35c951aec78a74d6597a
mohamed-saad <mahameds19999@gmail.com>
2019-01-23 11:48:08 +0200

    add content to register component

--- angular-src/src/app/register/register.component.ts
+++ angular-src/src/app/register/register.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  name: String;
+  username: String;
+  email: String;
+  password: String;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    const {name, username, email, password} = this;
   }
 
 }
